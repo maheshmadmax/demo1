c48d9c855870156d776427628c79846f0b22b12b
root <root@ip-172-31-36-196.ap-south-1.compute.internal>
2020-05-11 05:31:13 +0000

    demo1

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ app.get('/', function (req, res) {
   res.send('hello world');
 });
 
-console.log('Visit http://localhost:5000/')
+console.log('Visit http://localhost:5005/')
 console.log('Press Ctrl+C to shut down...')
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5005);
 
 module.exports = app;
